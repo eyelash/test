3ed3eade0012e568f081838e6cb950240889a93e
Elias Aebi <eyelash@users.noreply.github.com>
2020-01-19 10:22:54 +0100

    use double quotes

--- react-typescript/webpack.config.js
+++ react-typescript/webpack.config.js
@@ -1,19 +1,19 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: './src/main.tsx',
+  entry: "./src/main.tsx",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/
       }
     ]
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: [".ts", ".tsx", ".js"]
   },
   plugins: [
     new HtmlWebpackPlugin(),
