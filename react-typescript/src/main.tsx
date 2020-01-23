edf52da7abe5302f9f66259afcf0d870f3af7388
Elias Aebi <eyelash@users.noreply.github.com>
2020-01-23 17:00:06 +0100

    CSS

--- react-typescript/src/main.tsx
+++ react-typescript/src/main.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { CustomComponent } from "./CustomComponent";
+import "./style.css";
 
 ReactDOM.render(<CustomComponent/>, document.getElementById("root"));
